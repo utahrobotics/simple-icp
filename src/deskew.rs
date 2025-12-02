7067fb312a2f833b48bf9ebb3a98b789c17d135a
Matthew Ashton Knochel <matthewashtonknochel@gmail.com>
2025-12-01 18:04:21 -0700

    reference frame fix

--- src/deskew.rs
+++ src/deskew.rs
@@ -1,7 +1,5 @@
 use crate::point3d::Point3d;
 
-/// timestamps are all in seconds since start of the frame
-/// Assumes timestamped_poses is sorted by timestamp
 pub fn deskew_scan(
     cloud: &mut [Point3d],
     timestamped_poses: &Vec<(nalgebra::Isometry3<f64>, f64)>,
@@ -10,13 +8,13 @@ pub fn deskew_scan(
         return;
     }
 
+    let reference_pose = &timestamped_poses[0].0;
+
     for point in cloud.iter_mut() {
-        // Find the two poses surrounding the point's timestamp
         let mut before_pose = &timestamped_poses[0].0;
         let mut after_pose = &timestamped_poses[0].0;
-
-        let mut before_pose_time = timestamped_poses[0].1;
-        let mut after_pose_time = timestamped_poses[0].1;
+        let mut before_time = timestamped_poses[0].1;
+        let mut after_time = timestamped_poses[0].1;
 
         for i in 0..timestamped_poses.len() - 1 {
             if point.timestamp >= timestamped_poses[i].1
@@ -24,26 +22,25 @@ pub fn deskew_scan(
             {
                 before_pose = &timestamped_poses[i].0;
                 after_pose = &timestamped_poses[i + 1].0;
-                before_pose_time = timestamped_poses[i].1;
-                after_pose_time = timestamped_poses[i + 1].1;
+                before_time = timestamped_poses[i].1;
+                after_time = timestamped_poses[i + 1].1;
                 break;
             }
         }
 
-        let alpha = (point.timestamp - before_pose_time) / (after_pose_time - before_pose_time);
-
+        let alpha = (point.timestamp - before_time) / (after_time - before_time);
         let interpolated_translation =
             before_pose.translation.vector * (1.0 - alpha) + after_pose.translation.vector * alpha;
         let interpolated_rotation = before_pose.rotation.slerp(&after_pose.rotation, alpha);
-
-        let interpolated_pose = nalgebra::Isometry3::from_parts(
+        let sensor_pose_at_point_time = nalgebra::Isometry3::from_parts(
             nalgebra::Translation3::from(interpolated_translation),
             interpolated_rotation,
         );
 
-        // Transform the point using the interpolated pose
+        // point_in_reference = reference_pose^-1 * sensor_pose_at_time * point
         let original_point = nalgebra::Point3::new(point.x as f64, point.y as f64, point.z as f64);
-        let deskewed_point = interpolated_pose * original_point;
+        let transform = reference_pose.inverse() * sensor_pose_at_point_time;
+        let deskewed_point = transform * original_point;
 
         point.x = deskewed_point.x as f32;
         point.y = deskewed_point.y as f32;
